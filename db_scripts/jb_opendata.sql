4853adef27d2c092214a814fd977a5699209fe43
DaveBathnes <dxrowe@somerset.gov.uk>
2017-03-28 16:42:07 +0100

    starting open data job

---
+++ db_scripts/jb_opendata.sql
@@ -0,0 +1,39 @@
+--------------------------------------------------------------------
+-- job: open data
+-- this set of SQL scripts exports libraries west open data
+-- to a set folder.  this is intended to be run on a nightly basis
+-- to provide regular extracts which will be published to the 
+-- libraries west open data repository.
+-- this script assumes a directory on the c drive called opendata
+--------------------------------------------------------------------
+
+-- 1.  Users by mutiple deprivation index and authority
+
+
+
+
+-- 2.  Users by ward and authority
+
+
+
+
+-- 3.  Issues by day, by item type, by library
+
+
+
+
+-- 4.  Current transits
+-- The current transits export is one that should not overwrite previous data as it is
+-- a snapshot of the current situation.  for each time it's run create a datestamped file
+copy(select * from vw_transits) to "c:\opendata\lms\transits\transits.csv" csv
+
+
+
+-- 5.  Bills by month by library by type
+
+
+
+
+-- 6.  Holds by day, by item type, by library, by user multiple deprivation index
+
+
