6be0575112fa7530d715374bb3e9c4820dde6f6c
Dave <dave_roweuk@yahoo.co.uk>
2017-04-26 21:23:01 +0100

    change to catalogue view and borrowers by library

--- db_scripts/vw_catalogue.sql
+++ db_scripts/vw_catalogue.sql
@@ -9,7 +9,7 @@ select
     c.flexible_key as flex_key,
     ( select (string_to_array(marc.tag, '~'::text))[1] as string_to_array from marc where marc.marc = c.marc and marc.tag_number::text = '245'::text limit 1) as title,
     ( select (string_to_array(marc.tag, '~'::text))[1] as string_to_array from marc where marc.marc = c.marc and marc.tag_number::text = '100'::text limit 1) as author,
-    ( select (string_to_array(marc.tag, ' '::text))[1] as string_to_array from marc where marc.marc = c.marc and marc.tag_number::text = '020'::text limit 1) as isbn,
+    ( select (string_to_array(marc.tag, ' '::text))[1] as string_to_array from marc where marc.marc = c.marc and marc.tag_number::text = '20'::text limit 1) as isbn,
     c.year_of_publication as published,
     pf.policy_name as format,
     ( select count(item.catalogue_key) as count from item where item.catalogue_key = c.catalogue_key) as items,
