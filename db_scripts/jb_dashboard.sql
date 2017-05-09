cfff03a9ebdf601f8f0f6e2081e3b2c3377f06d9
DaveBathnes <dxrowe@somerset.gov.uk>
2017-05-09 14:52:51 +0100

    Adding issues by library dashboard view

--- db_scripts/jb_dashboard.sql
+++ db_scripts/jb_dashboard.sql
@@ -5,6 +5,7 @@
 
 copy(select * from vw_dashboard_holds) to 'c:\dbdata\dashboard\holds.csv' csv;
 copy(select * from vw_dashboard_issues) to 'c:\dbdata\dashboard\issues.csv' csv;
+copy(select * from vw_dashboard_issuesbylibrary) to 'c:\dbdata\dashboard\issuesbylibrary.csv' csv;
 copy(select * from vw_dashboard_residentusers) to 'c:\dbdata\dashboard\residentusers.csv' csv;
 copy(select * from vw_dashboard_borrowersbylibrary) to 'c:\dbdata\dashboard\borrowersbylibrary.csv' csv;
 copy(select * from vw_dashboard_borrowersbyauthority) to 'c:\dbdata\dashboard\borrowersbyauthority.csv' csv;
