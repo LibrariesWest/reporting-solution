d6a733c18b85c5f5145fa00bf7bc06387a1f4042
Dave <dave_roweuk@yahoo.co.uk>
2017-05-09 23:15:48 +0100

    Changes to issues by ward view

---
+++ db_scripts/vw_dashboard_issuesbyward_geo.sql
@@ -0,0 +1,44 @@
+---------------------------------------------------------------
+-- view: vw_dashboard_issuesbyward_geo
+---------------------------------------------------------------
+
+-- drop view vw_dashboard_issuesbyward_geo;
+create view vw_dashboard_issuesbyward_geo as 
+select row_to_json(fc)
+from (
+	select 'FeatureCollection' As type, array_to_json(array_agg(f)) as features
+	from (
+		select 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((select l from (select ward, issues) As l)) as properties
+		from (
+			select
+			ow.name as ward,
+			wc.issues,
+			ST_SimplifyPreserveTopology(ow.geom, 0.002) as geom
+			from
+				(select
+				op.admin_ward_code as ward_code,
+				count(chp.user_key) as issues
+				from
+					(select 
+					ch.user_key,
+					case
+						when u.mailing_address = 1 then ( select userxinfo.entry from userxinfo where userxinfo."offset" = u.address_offset_1 and userxinfo.entry_number = 9000 limit 1)
+						when u.mailing_address = 2 then ( select userxinfo.entry from userxinfo where userxinfo."offset" = u.address_offset_2 and userxinfo.entry_number = 9036 limit 1)
+						when u.mailing_address = 3 then ( select userxinfo.entry from userxinfo where userxinfo."offset" = u.address_offset_3 and userxinfo.entry_number = 9036 limit 1)
+						else null::text
+					end as postcode
+					from
+						(select user_key from charge where date_charged > (now() - interval '1 year')
+						union 
+						select user_key from chargehist where date_charged > (now() - interval '1 year')) ch
+					join users u
+					on ch.user_key = u.user_key) chp
+				join os_postcodes op
+				on replace(op.postcode, ' ', '') = replace(upper(chp.postcode), ' ', '')
+				group by op.admin_ward_code
+				having count(chp.user_key) > 10) wc
+			join os_wards ow
+			on ow.code = wc.ward_code
+		) as lg
+	) as f
+)  As fc;
