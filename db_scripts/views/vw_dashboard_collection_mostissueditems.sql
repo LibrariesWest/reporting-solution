26bf38cf723a0d9569ac78b498a16d53b1168fd5
Dave <dave_roweuk@yahoo.co.uk>
2017-11-07 22:03:52 +0000

    Bug in discards table

--- db_scripts/views/vw_dashboard_collection_mostissueditems.sql
+++ db_scripts/views/vw_dashboard_collection_mostissueditems.sql
@@ -5,7 +5,7 @@
 -- drop view vw_dashboard_collection_mostissueditems;
 create or replace view vw_dashboard_collection_mostissueditems as 
 select 
-    i.item_id,
+    i.id,
     c.title,
     c.isbn,
     count(ch.key) as issues,
@@ -14,6 +14,6 @@ from vw_charges_chargeshistory as ch
 join item i on i.catalogue_key = ch.catalogue_key and i.call_sequence = ch.call_sequence and i.copy_number = ch.copy_number
 join vw_catalogue c on c.catalogue_key = i.catalogue_key
 where ch.date_charged > (now() - interval '1 year')
-group by i.item_id, c.title, c.isbn
+group by i.id, c.title, c.isbn
 order by issues desc
 limit 100;
