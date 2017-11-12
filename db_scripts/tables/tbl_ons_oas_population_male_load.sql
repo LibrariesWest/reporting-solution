ce6ae0cce1cfe2a677527bd663cfc93b3fa38512
Dave <dave_roweuk@yahoo.co.uk>
2017-11-12 22:33:42 +0000

    updating to ensure male population counts load

--- db_scripts/tables/tbl_ons_oas_population_male_load.sql
+++ db_scripts/tables/tbl_ons_oas_population_male_load.sql
@@ -2,3 +2,5 @@
 -- table: ons_oas_population_male
 ---------------------------------------------------------------
 
+truncate ons_oas_population_male;
+copy ons_oas_population_male from 'c:\dbdata\ons\OA_SW_Mid2015Males.csv' delimiter ',' csv header;
