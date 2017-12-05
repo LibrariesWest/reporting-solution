470073a2e90d226f746ab44e15a482cb6b8e7c53
Dave <dave_roweuk@yahoo.co.uk>
2017-12-05 16:08:16 +0000

    splitting up open data items per authority

--- db_scripts/jobs/jb_opendata.sql
+++ db_scripts/jobs/jb_opendata.sql
@@ -9,7 +9,12 @@ copy(select * from vw_opendata_cashmanagement) to 'c:\dbdata\opendata\cashmanage
 copy(select * from vw_opendata_catalogue) to 'c:\dbdata\opendata\catalogue.csv' csv header;
 copy(select * from vw_opendata_holds) to 'c:\dbdata\opendata\holds.csv' csv header;
 copy(select * from vw_opendata_issues) to 'c:\dbdata\opendata\issues.csv' csv header;
-copy(select * from vw_opendata_items) to 'c:\dbdata\opendata\items.csv' csv header;
+copy(select * from vw_opendata_items where authority = 'Bath and North East Somerset') to 'c:\dbdata\opendata\items_banes.csv' csv header;
+copy(select * from vw_opendata_items where authority = 'Bristol') to 'c:\dbdata\opendata\items_bristol.csv' csv header;
+copy(select * from vw_opendata_items where authority = 'Dorset') to 'c:\dbdata\opendata\items_dorset.csv' csv header;
+copy(select * from vw_opendata_items where authority = 'North Somerset') to 'c:\dbdata\opendata\items_northsomerset.csv' csv header;
+copy(select * from vw_opendata_items where authority = 'Poole') to 'c:\dbdata\opendata\items_poole.csv' csv header;
+copy(select * from vw_opendata_items where authority = 'Somerset') to 'c:\dbdata\opendata\items_somerset.csv' csv header;
+copy(select * from vw_opendata_items where authority = 'South Gloucestershire') to 'c:\dbdata\opendata\items_southglos.csv' csv header;
 copy(select * from vw_opendata_members) to 'c:\dbdata\opendata\members.csv' csv header;
 copy(select * from vw_opendata_transits) to 'c:\dbdata\opendata\transits.csv' csv header;
-
