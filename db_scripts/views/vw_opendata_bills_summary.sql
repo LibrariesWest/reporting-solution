39352f662e155c3fb36cb811e835745f4c394095
DaveBathnes <dave_roweuk@yahoo.co.uk>
2018-01-16 16:28:49 +0000

    Rounding in the bills summary

--- db_scripts/views/vw_opendata_bills_summary.sql
+++ db_scripts/views/vw_opendata_bills_summary.sql
@@ -8,8 +8,8 @@ select
     to_char(b.date_billed, 'YYYY') as year_billed,
     b.bill_authority as bill_authority,
     b.reason as bill_reason,
-    count(*) as number_of_bills,
-    sum(amount) as total_billed
+    round(count(*), -1) as number_of_bills,
+    round(sum(amount), -1) as total_billed
 from vw_bills b
 where b.date_billed >= '7-Jun-2016'
 -- filter out some libraries - acquisitions etc
