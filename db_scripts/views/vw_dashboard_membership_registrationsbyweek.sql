37f8e00721b01fd0f00ed37e43183ed011acdd98
Dave <dave_roweuk@yahoo.co.uk>
2018-01-17 15:00:38 +0000

    correcting registrations by week

--- db_scripts/views/vw_dashboard_membership_registrationsbyweek.sql
+++ db_scripts/views/vw_dashboard_membership_registrationsbyweek.sql
@@ -5,10 +5,11 @@
 -- drop view vw_dashboard_membership_registrationsbyweek;
 create or replace view vw_dashboard_membership_registrationsbyweek as
 select
-    date_trunc('week', date_created) as week,
+    to_char(date_trunc('week', date_created), 'YYYYMMdd') as week_beginning,
     authority,
     library,
     count(*)
 from vw_users
+where date_created > (now() - interval '2 years')
 group by date_trunc('week', date_created), authority, library
 order by date_trunc('week', date_created), authority, library;
