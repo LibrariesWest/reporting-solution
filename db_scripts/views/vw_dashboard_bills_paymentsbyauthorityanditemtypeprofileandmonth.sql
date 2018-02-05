c91e5a006cd7feccb502eb5168b914d76ebc1a61
DaveBathnes <dave_roweuk@yahoo.co.uk>
2018-02-05 16:10:34 +0000

    adding payments by profile

---
+++ db_scripts/views/vw_dashboard_bills_paymentsbyauthorityanditemtypeprofileandmonth.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------------------------
+-- view: vw_dashboard_bills_paymentsbyauthorityanditemtypeprofileandmonth
+--------------------------------------------------------------------------
+
+-- drop view vw_dashboard_bills_paymentsbyauthorityanditemtypeprofileandmonth;
+create or replace view vw_dashboard_bills_paymentsbyauthorityanditemtypeprofileandmonth as
+select
+    bp.payment_authority,
+    to_char(bp.payment_date, 'YYYYMM') as payment_month,
+    bp.item_authority,
+    bp.item_type,
+    bp.reason,
+    bp.payment_type,
+    vu.profile,
+    count(bp.bill_payment_key) as number_of_payments,
+    sum(bp.payment_amount) as total_paid
+from vw_bills_billpayments bp
+join vw_users vu on bp.user_key = vu.user_key
+group by payment_month, bp.payment_authority, bp.reason, bp.payment_type, bp.item_authority, bp.item_type, bp.payment_type, vu.profile
+order by payment_month, bp.payment_authority, bp.reason, bp.payment_type, bp.item_authority, bp.item_type, bp.payment_type, vu.profile;
