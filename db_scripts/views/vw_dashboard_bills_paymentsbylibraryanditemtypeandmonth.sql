0c25267050b25dbf31421b7e52d93155233b8489
DaveBathnes <dave_roweuk@yahoo.co.uk>
2018-03-05 16:21:55 +0000

    corrections to bill payments by library

--- db_scripts/views/vw_dashboard_bills_paymentsbylibraryanditemtypeandmonth.sql
+++ db_scripts/views/vw_dashboard_bills_paymentsbylibraryanditemtypeandmonth.sql
@@ -6,7 +6,7 @@
 create or replace view vw_dashboard_bills_paymentsbylibraryanditemtypeandmonth as
 select
     bp.payment_authority,
-    bp.library,
+    bp.payment_library,
     to_char(bp.payment_date, 'YYYYMM') as payment_month,
     bp.item_authority,
     bp.item_type,
@@ -15,5 +15,5 @@ select
     count(bp.bill_payment_key) as number_of_payments,
     sum(bp.payment_amount) as total_paid
 from vw_bills_billpayments bp
-group by payment_month, bp.payment_authority, bp.library, bp.reason, bp.payment_type, bp.item_authority, bp.item_type, bp.payment_type
-order by payment_month, bp.payment_authority, bp.library, bp.reason, bp.payment_type, bp.item_authority, bp.item_type, bp.payment_type;
+group by payment_month, bp.payment_authority, bp.payment_library, bp.reason, bp.payment_type, bp.item_authority, bp.item_type, bp.payment_type
+order by payment_month, bp.payment_authority, bp.payment_library, bp.reason, bp.payment_type, bp.item_authority, bp.item_type, bp.payment_type;
--- db_scripts/views/vw_dashboard_bills_paymentsbylibraryanditemtypeprofileandmonth.sql
+++ db_scripts/views/vw_dashboard_bills_paymentsbylibraryanditemtypeprofileandmonth.sql
@@ -6,7 +6,7 @@
 create or replace view vw_dashboard_bills_paymentsbylibraryanditemtypeprofileandmonth as
 select
     bp.payment_authority,
-    bp.library,
+    bp.payment_library,
     to_char(bp.payment_date, 'YYYYMM') as payment_month,
     bp.item_authority,
     bp.item_type,
@@ -17,5 +17,5 @@ select
     sum(bp.payment_amount) as total_paid
 from vw_bills_billpayments bp
 join vw_users vu on bp.user_key = vu.user_key
-group by payment_month, bp.payment_authority, bp.library, bp.reason, bp.payment_type, bp.item_authority, bp.item_type, bp.payment_type, vu.profile
-order by payment_month, bp.payment_authority, bp.library, bp.reason, bp.payment_type, bp.item_authority, bp.item_type, bp.payment_type, vu.profile;
+group by payment_month, bp.payment_authority, bp.payment_library, bp.reason, bp.payment_type, bp.item_authority, bp.item_type, bp.payment_type, vu.profile
+order by payment_month, bp.payment_authority, bp.payment_library, bp.reason, bp.payment_type, bp.item_authority, bp.item_type, bp.payment_type, vu.profile;
