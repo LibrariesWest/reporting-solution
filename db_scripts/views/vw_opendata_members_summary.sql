2f54f961482e75a126869f6d4188d08dcacee3ff
DaveBathnes <dave_roweuk@yahoo.co.uk>
2018-10-24 13:46:37 +0100

    Updating members summary view

--- db_scripts/views/vw_opendata_members_summary.sql
+++ db_scripts/views/vw_opendata_members_summary.sql
@@ -6,9 +6,12 @@
 create or replace view vw_opendata_members_summary as
 select 
 	authority as registered_authority,
+	libraries.name,
 	round(count(*), -1) as users
 from vw_users
+join libraries on libraries.code = vw_users.library
 where last_activity_date >= (now() - interval '1 year')
 and library in (select code from libraries)
-group by authority
-order by authority;
+group by authority, libraries.name
+having count(*) > 100
+order by authority, libraries.name;
